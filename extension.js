65856681065fc87dbf3ea74b7084861299b88851
Leon Kasovan <leonkasovan@gmail.com>
2025-06-16 05:14:18 +0700

    Tags version 1.2.0

--- extension.js
+++ extension.js
@@ -1,12 +1,19 @@
 // This file is part of the template-picker extension for Visual Studio Code.
 // It provides functionality to extract files from a ZIP archive and create a workspace from templates.
+// Prepare in MYSYS2 with the following commands:
+// pacman -S mingw-w64-ucrt-x86_64-nodejs
+// npm install
+// npm install -g @vscode/vsce
+// vsce package
+// vsce publish
+
 // Developed by Dhani Novan (leonkasovan@gmail.com) Jakarta, 30 Mei 2025
 const vscode = require('vscode');
 const path = require('path');
 const templates = require('./moduleTemplate');
 const { registerAllCommands } = require('./commands');
 const downloader = require('./commands/downloadTemplate');
-  
+
 function activate(context) {
 	// Installed panel (from zip files in 'templates' folder)
 	const installedViewProvider = new templates.InstalledTemplateProvider(path.join(context.extensionPath, 'templates'));
